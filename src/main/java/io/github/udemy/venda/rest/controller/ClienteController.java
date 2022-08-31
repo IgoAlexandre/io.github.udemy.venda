457d893548b610349eb717029bf590d271822b05
Igo Alexandre <igo.alexandre@gmail.com>
2022-08-30 23:05:34 -0300

    Refatorando Controller Cliente

--- src/main/java/io/github/udemy/venda/rest/controller/ClienteController.java
+++ src/main/java/io/github/udemy/venda/rest/controller/ClienteController.java
@@ -2,20 +2,16 @@ package io.github.udemy.venda.rest.controller;
 
 import io.github.udemy.venda.domain.entity.Cliente;
 import io.github.udemy.venda.domain.repository.ClienteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
 
-@Controller
-@RequestMapping()
+@RestController
+@RequestMapping("/api/clientes")
 public class ClienteController {
 
     private ClienteRepository clienteRepository;
@@ -30,60 +26,51 @@ public class ClienteController {
             consumes = {"application/json", "application/xml"}, //forma como pode receber a solicitação
             produces = {"application/json", "application/xml"} //forma como vai retornar o resultado
     )
-    @ResponseBody
     public String helloCliente(@PathVariable("nomeCliente") String nomeCliente){
         return String.format("Hello %s", nomeCliente);
     }
 
-    @GetMapping("/api/clientes/{idCliente}")
-    @ResponseBody
-    public ResponseEntity<Cliente> getClienteByIdCliente(@PathVariable Integer idCliente){
-        Optional<Cliente> cliente = clienteRepository.findById(idCliente);
-
-        if(cliente.isPresent()){
-            return ResponseEntity.ok(cliente.get());
-        }
-
-        return ResponseEntity.notFound().build();
+    @GetMapping("{idCliente}")
+    public Cliente getClienteByIdCliente(@PathVariable Integer idCliente){
+        return clienteRepository
+                .findById(idCliente)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado")
+            );
     }
 
-    @PostMapping("/api/clientes")
-    @ResponseBody
-    public ResponseEntity<Cliente> salvarCliente(@RequestBody Cliente cliente){
-        Cliente clienteSalvo = clienteRepository.save(cliente);
-        return ResponseEntity.ok(clienteSalvo);
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente salvarCliente(@RequestBody Cliente cliente){
+        return clienteRepository.save(cliente);
     }
 
-    @DeleteMapping("/api/clientes/{idCliente}")
-    @ResponseBody
-    public ResponseEntity<Cliente> deletarCliente(@PathVariable Integer idCliente){
-        Optional<Cliente> cliente = clienteRepository.findById(idCliente);
-
-        if(cliente.isPresent()){
-            clienteRepository.delete(cliente.get());
-            return ResponseEntity.noContent().build();
-        }
-
-        return ResponseEntity.notFound().build();
+    @DeleteMapping("{idCliente}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarCliente(@PathVariable Integer idCliente){
+         clienteRepository.findById(idCliente)
+                .map(cliente -> {
+                    clienteRepository.delete(cliente);
+                    return cliente;
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado")
+        );
     }
 
-    @PutMapping("/api/clientes/{idCliente}")
-    @ResponseBody
-    public ResponseEntity atualizarCliente(@PathVariable Integer idCliente, @RequestBody Cliente cliente){
-        return clienteRepository.findById(idCliente)
+    @PutMapping("{idCliente}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizarCliente(@PathVariable Integer idCliente, @RequestBody Cliente cliente){
+         clienteRepository.findById(idCliente)
                 .map(clienteExistente -> {
                     cliente.setIdCliente(clienteExistente.getIdCliente());
                     clienteRepository.save(cliente);
-                    return ResponseEntity.noContent().build();
+                    return cliente;
                 })
-                .orElseGet(() -> ResponseEntity.notFound().build());
-
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
     }
 
 
-    @GetMapping("/api/clientes")
-    @ResponseBody
-    public ResponseEntity find(Cliente clienteFiltro){
+    @GetMapping
+    public List<Cliente> find(Cliente clienteFiltro){
         ExampleMatcher matcher = ExampleMatcher
                                     .matching()
                                     .withIgnoreCase()
@@ -91,7 +78,6 @@ public class ClienteController {
                                             ExampleMatcher.StringMatcher.CONTAINING);
 
         Example example = Example.of(clienteFiltro, matcher);
-        List<Cliente> listaClientes = clienteRepository.findAll(example);
-        return ResponseEntity.ok(listaClientes);
+        return clienteRepository.findAll(example);
     }
 }
