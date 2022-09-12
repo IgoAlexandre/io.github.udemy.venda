409065a208b425ff2859e32100f819662c7c28df
Igo Alexandre <igo.alexandre@gmail.com>
2022-09-12 20:30:22 -0300

    Spring Security inMemoryAuthentication

---
+++ src/main/java/io/github/udemy/venda/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,15 @@
+package io.github.udemy.venda.service.impl;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioServiceImpl implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
