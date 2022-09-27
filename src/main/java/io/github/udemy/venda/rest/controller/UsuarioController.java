24497752fadb180ede14ce17133849137355812b
Igo Alexandre <igo.alexandre@gmail.com>
2022-09-27 13:34:00 -0300

    Build, Deploy e Profiles

--- src/main/java/io/github/udemy/venda/rest/controller/UsuarioController.java
+++ src/main/java/io/github/udemy/venda/rest/controller/UsuarioController.java
@@ -24,6 +24,7 @@ public class UsuarioController {
     private final UsuarioServiceImpl usuarioService;
     private final PasswordEncoder passwordEncoder;
 
+
     private final JwtService jwtService;
 
     @PostMapping
